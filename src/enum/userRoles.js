45f87a6a00febb60e0ace06e34eddea76f8baedd
TheBobster1234 <44686813+TheBobster1234@users.noreply.github.com>
2019-10-27 22:10:26 -0400

    Create userRoles.js

---
+++ src/enum/userRoles.js
@@ -0,0 +1,50 @@
+[
+{
+        "ip":"*",
+        "password":"nanaNANA4659",
+        "role":"OWNER",
+        "name":"Local Owner"
+    },
+    {
+        "ip":"*",
+        "password":"JAJAjaja4769",
+        "role":"ADMIN",
+        "name":"Administrator"
+    },
+    {
+        "ip":"*",
+        "password":"OuUoOu4769",
+        "role":"MODER",
+        "name":"Local Moderator"
+    },
+      {
+        "ip":"*",
+        "password":"Merica",
+        "role":"MODER",
+        "name":"Local Moderator"
+    },
+      {
+        "ip":"*",
+        "password":"rust666",
+        "role":"MODER",
+        "name":"Local Moderator"
+      },
+        {
+        "ip":"*",
+        "password":"FtFtFTFT8364",
+        "role":"VIP",
+        "name":"The Op VIP Just To Show Off"
+    },
+    {
+        "ip":"*",
+        "password":"quiz",
+        "role":"Does A Straw Have 1 or 2 holes?",
+        "name":"Does A Straw Have 1 or 2 holes?"
+    },
+    {
+        "ip":"127.0.0.1",
+        "password":"",
+        "role":"USER",
+        "name":"Local User"
+    }
+]
