6322841ba25c713b2173073cc90ca447cc03af5f
TheBobster1234 <44686813+TheBobster1234@users.noreply.github.com>
2019-10-27 22:19:17 -0400

    Update SetBorder.js

--- src/packet/SetBorder.js
+++ src/packet/SetBorder.js
@@ -1,40 +1,37 @@
-function ServerStat(playerTracker) {
+function SetBorder(playerTracker, border, gameType, serverName) {
     this.playerTracker = playerTracker;
+    this.border = border;
+    this.gameType = gameType;
+    this.serverName = serverName;
 }
 
-module.exports = ServerStat;
+module.exports = SetBorder;
 
-ServerStat.prototype.build = function (protocol) {
-    var gameServer = this.playerTracker.gameServer;
-    // Get server statistics
-    var totalPlayers = 0;
-    var alivePlayers = 0;
-    var spectPlayers = 0;
-    for (var i = 0; i < gameServer.clients.length; i++) {
-        var socket = gameServer.clients[i];
-        if (socket == null || !socket.isConnected)
-            continue;
-        totalPlayers++;
-        if (socket.playerTracker.cells.length > 0)
-            alivePlayers++;
-        else
-            spectPlayers++;
+SetBorder.prototype.build = function (protocol) {
+    var scrambleX = this.playerTracker.scrambleX;
+    var scrambleY = this.playerTracker.scrambleY;
+    if (this.gameType == null) {
+        var buffer = Buffer.alloc(33);
+        buffer.writeUInt8(0x40, 0, true);
+        buffer.writeDoubleLE(this.border.minx + scrambleX, 1, true);
+        buffer.writeDoubleLE(this.border.miny + scrambleY, 9, true);
+        buffer.writeDoubleLE(this.border.maxx + scrambleX, 17, true);
+        buffer.writeDoubleLE(this.border.maxy + scrambleY, 25, true);
+        return buffer;
     }
-    var obj = {
-        'name': gameServer.config.serverName,
-        'mode': gameServer.gameMode.name,
-        'uptime': Math.round((gameServer.stepDateTime - gameServer.startTime) / 1000),
-        'update': gameServer.updateTimeAvg.toFixed(3),
-        'playersTotal': totalPlayers,
-        'playersAlive': alivePlayers,
-        'playersSpect': spectPlayers,
-        'playersLimit': gameServer.config.serverMaxConnections
-    };
-    var json = JSON.stringify(obj);
-    // Serialize
     var BinaryWriter = require("./BinaryWriter");
     var writer = new BinaryWriter();
-    writer.writeUInt8(254);             // Message Id
-    writer.writeStringZeroUtf8(json);   // JSON
+    writer.writeUInt8(0x40);                                // Packet ID
+    writer.writeDouble(this.border.minx + scrambleX);
+    writer.writeDouble(this.border.miny + scrambleY);
+    writer.writeDouble(this.border.maxx + scrambleX);
+    writer.writeDouble(this.border.maxy + scrambleY);
+    writer.writeUInt32(this.gameType >> 0);
+    var name = this.serverName;
+    if (name == null) name = "";
+    if (protocol < 6)
+        writer.writeStringZeroUnicode(name);
+    else
+        writer.writeStringZeroUtf8(name);
     return writer.toBuffer();
 };
